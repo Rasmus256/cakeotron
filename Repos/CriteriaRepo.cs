8aa5464db4bd3eb37267d3a1803623c856a3a1f1
Rasmus256 <rasmus256@gmail.com>
2023-06-13 09:53:41 +0200

    Update CriteriaRepo.cs

--- Repos/CriteriaRepo.cs
+++ Repos/CriteriaRepo.cs
@@ -12,7 +12,7 @@ namespace CakeOTron.Service
             var digits = Enumerable.Range(0,10);
             for (int i = 1; i < 10; i++){
                 for (int j = 1; j<6; j++) {
-                    SignificantNumbers.Add(Math.Power(i,j));
+                    SignificantNumbers.Add(Math.Pow(i,j));
                 }
             }
             List<string> significantStrings = new List<string> { };
