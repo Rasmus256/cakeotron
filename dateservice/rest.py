1869b49a4b743b2351ed8419752acacdb6b13400
Rasmus256 <rasmus256@gmail.com>
2024-03-01 12:13:34 +0100

    Alters date service

--- dateservice/rest.py
+++ dateservice/rest.py
@@ -25,46 +25,52 @@ response_404 = """
 @app.exception_handler(404)
 async def custom_404_handler(_, __):
     return HTMLResponse(response_404)
+    
+dates = {1:{ 'Description': 'RDP Birthday',
+            'Date': datetime.datetime(1991, 6, 20)},
+        2:{'Description': "Maj Birthday",
+            'Date': datetime.datetime(1991, 5, 15)},
+        3:{'Description': "Mor Kirsten Birthday",
+            'Date': datetime.datetime(1961, 5, 23)},
+        4:{'Description': "Far Frank Birthday",
+            'Date': datetime.datetime(1960, 3, 25)},
+        5:{'Description': "Alberte Birthday",
+            'Date': datetime.datetime(2017, 3, 4)},
+        6:{'Description': "Gry Birthday",
+            'Date': datetime.datetime(2018, 12, 22)},
+        7:{'Description': "Mathias Birthday",
+            'Date': datetime.datetime(1992, 4, 24)},
+        8:{'Description': "Stefan Birthday",
+            'Date': datetime.datetime(1991, 9, 11)},
+        9:{'Description': "Wedding Anniversary",
+            'Date': datetime.datetime(2016, 2, 12)},
+        10:{ 'Description': "Kærestedag", 'Date': datetime.datetime(2012, 8, 12)},
+        11:{ 'Description': "Ansat ved Trifork",
+            'Date': datetime.datetime(2019, 1, 3)},
+        12:{ 'Description': "Maj Ansat ved VBC",
+            'Date': datetime.datetime(2021, 7, 1)},
+        13:{ 'Description': "Frodos fødselsdag",
+            'Date': datetime.datetime(2015, 9, 7)},
+        14:{ 'Description': "Morten Mikkelsens fødselsdag",
+            'Date': datetime.datetime(1992, 9, 7)},
+        15:{ 'Description': "Idas fødselsdag",
+            'Date': datetime.datetime(1992, 7, 14)},
+        16:{ 'Description': "Bos fødselsdag",
+            'Date': datetime.datetime(1998, 2, 15)},
+        17:{ 'Description': "Ansat ved Bankdata",
+            'Date': datetime.datetime(2022, 12, 1)},
+        18:{ 'Description': "Frank og Kirsten Bryllupsdag",
+            'Date': datetime.datetime(1988, 6, 25)}
+}
+@app.get("/dates/{id}")
+async def get_date_by_id(id):
+    global dates
+    return dates[id]
 
 @app.get("/dates")
 async def get_dates():
-    return [
-        {'Description': 'RDP Birthday',
-            'Date': datetime.datetime(1991, 6, 20)},
-        {'Description': "Maj Birthday",
-            'Date': datetime.datetime(1991, 5, 15)},
-        {'Description': "Mor Kirsten Birthday",
-            'Date': datetime.datetime(1961, 5, 23)},
-        {'Description': "Far Frank Birthday",
-            'Date': datetime.datetime(1960, 3, 25)},
-        {'Description': "Alberte Birthday",
-            'Date': datetime.datetime(2017, 3, 4)},
-        {'Description': "Gry Birthday",
-            'Date': datetime.datetime(2018, 12, 22)},
-        {'Description': "Mathias Birthday",
-            'Date': datetime.datetime(1992, 4, 24)},
-        {'Description': "Stefan Birthday",
-            'Date': datetime.datetime(1991, 9, 11)},
-        {'Description': "Wedding Anniversary",
-            'Date': datetime.datetime(2016, 2, 12)},
-        {'Description': "Kærestedag", 'Date': datetime.datetime(2012, 8, 12)},
-        {'Description': "Ansat ved Trifork",
-            'Date': datetime.datetime(2019, 1, 3)},
-        {'Description': "Maj Ansat ved VBC",
-            'Date': datetime.datetime(2021, 7, 1)},
-        {'Description': "Frodos fødselsdag",
-            'Date': datetime.datetime(2015, 9, 7)},
-        {'Description': "Morten Mikkelsens fødselsdag",
-            'Date': datetime.datetime(2015, 9, 7)},
-        {'Description': "Idas fødselsdag",
-            'Date': datetime.datetime(1992, 7, 14)},
-        {'Description': "Bos fødselsdag",
-            'Date': datetime.datetime(1998, 2, 15)},
-        {'Description': "Ansat ved Bankdata",
-            'Date': datetime.datetime(2022, 12, 1)},
-        {'Description': "Frank og Kirsten Bryllupsdag",
-            'Date': datetime.datetime(1988, 6, 25)}
-    ]
+    global dates
+    return list(dates.keys())
 
 
 @app.get("/healthz")
