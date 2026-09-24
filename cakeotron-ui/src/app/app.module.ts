e434ed741a39b7d1d68bc343ff13994f0c70eca6
Rasmus256 <rasmus256@gmail.com>
2026-09-24 10:18:32 +0200

    Remove unused Angular module import and declarations

--- cakeotron-ui/src/app/app.module.ts
+++ cakeotron-ui/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [],
   imports: [BrowserModule, HttpClientModule, AppComponent],
   bootstrap: [AppComponent]
 })
